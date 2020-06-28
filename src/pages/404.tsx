39913248838d99dd12d8569d155027946e7592f7
Sascha Metz <metz@raum3.de>
2020-06-28 21:25:24 +0200

    Add 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,9 @@
-import React from "react"
-import Layout from "@components/layout"
-import SEO from "@components/seo"
+const NotFound = () => {
+  if (typeof window !== "undefined") {
+    window.location.href = "/"
+  }
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  return null
+}
 
-export default NotFoundPage
+export default NotFound

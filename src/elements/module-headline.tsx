b19135a7947b282ade0e6f5181c968805f45db90
Sascha Metz <metz@raum3.de>
2020-06-28 15:28:20 +0200

    Finished all Bands Module

---
+++ src/elements/module-headline.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { withClassName } from "@customTypes"
+
+export const ModuleHeadline: React.FC<withClassName> = ({ className = "" }) => {
+  return (
+    <h2
+      className={`text-6xl mb-8 flex items-center module-headline leading-none bg-grungy-light bg-contain bg-center bg-no-repeat ${className}`}
+    >
+      <span className="bg-grungy-primary bg-cover inline-block px-12">
+        Bands
+      </span>
+    </h2>
+  )
+}
--- src/modules/all-bands.tsx
+++ src/modules/all-bands.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import BandCard from "@components/band-card"
+import { ModuleHeadline } from "@elements"
 import { band } from "@customTypes"
 
 interface allBandsProps {
@@ -8,10 +9,13 @@ interface allBandsProps {
 
 const AllBands: React.FC<allBandsProps> = ({ bands }) => {
   return (
-    <section className="page-module container grid grid-cols-3 gap-10 items-stretch">
-      {bands.map((band, key) => (
-        <BandCard key={key} {...band} />
-      ))}
+    <section className="page-module container">
+      <ModuleHeadline>ModuleHeadline</ModuleHeadline>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10 items-stretch">
+        {bands.map((band, key) => (
+          <BandCard key={key} {...band} />
+        ))}
+      </div>
     </section>
   )
 }

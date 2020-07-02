570aea79c1908766848fd4780927500a0ced7244
Sascha Metz <metz@raum3.de>
2020-07-02 23:26:57 +0200

    Add conditional video hero

--- src/components/video-hero.tsx
+++ src/components/video-hero.tsx
@@ -1,18 +1,38 @@
 import React, { useLayoutEffect, useState } from "react"
 import TimelapseVideoHigh from "../videos/timelapse-720p.mp4"
 
+const useVideoURL = () => {
+  const [videoURL, setVideoURL] = useState("")
+  // Check if video should be loaded and then set videoURL
+  const checkVideo = () => {
+    if (window.outerWidth > 1024 && videoURL === "") {
+      setVideoURL(TimelapseVideoHigh)
+    }
+  }
+
+  useLayoutEffect(() => {
+    if (typeof window === "undefined") return
+    // Check initially
+    checkVideo()
+
+    // Check on resize if video isnt set yet
+    if (videoURL === "") window.addEventListener("resize", checkVideo)
+  }, [])
+  return videoURL
+}
+
 const VideoHero = () => {
+  const videoURL = useVideoURL()
   return (
     <header className="relative h-64 bg-black lg:h-auto">
-      <div className="hidden lg:block">
-        <video
-          autoPlay
-          muted
-          loop
-          className="min-h-screen object-cover w-full object-center"
-          src={TimelapseVideoHigh}
-        ></video>
-      </div>
+      <video
+        autoPlay
+        muted
+        loop
+        className="hidden lg:block min-h-screen object-cover w-full object-center"
+        src={videoURL}
+      ></video>
+
       <h1 className="text-4xl absolute-center text-center inline-block bg-grungy-primary bg-contain p-8 sm:text-6xl leading-none rounded-md max-w-lg">
         Schlacht um Otzenhausen
         <span className="hidden sm:block text-lg text-primary-400 mt-2">

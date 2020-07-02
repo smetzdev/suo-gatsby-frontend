aa59d333396e13fb70c5ba048ad4db006c8ebecc
Sascha Metz <metz@raum3.de>
2020-07-02 23:16:38 +0200

    Try something new with the video

--- src/components/video-hero.tsx
+++ src/components/video-hero.tsx
@@ -4,13 +4,15 @@ import TimelapseVideoHigh from "../videos/timelapse-720p.mp4"
 const VideoHero = () => {
   return (
     <header className="relative h-64 bg-black lg:h-auto">
-      {/* <video
-        autoPlay
-        muted
-        loop
-        className="hidden lg:block min-h-screen object-cover w-full object-center"
-        src={TimelapseVideoHigh}
-      ></video> */}
+      <div className="hidden lg-block">
+        <video
+          autoPlay
+          muted
+          loop
+          className="min-h-screen object-cover w-full object-center"
+          src={TimelapseVideoHigh}
+        ></video>
+      </div>
       <h1 className="text-4xl absolute-center text-center inline-block bg-grungy-primary bg-contain p-8 sm:text-6xl leading-none rounded-md max-w-lg">
         Schlacht um Otzenhausen
         <span className="hidden sm:block text-lg text-primary-400 mt-2">

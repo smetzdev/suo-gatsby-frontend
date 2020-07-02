68f696507e1ef4e433e3303cb94324c7e0efbe81
Sascha Metz <metz@raum3.de>
2020-07-02 21:31:16 +0200

    Update SUO Icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         background_color: `#1a202c`,
         theme_color: `#AD1F1F`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
         include_favicon: false,
       },
     },

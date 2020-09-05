1f9c3e7dc0cc61854f488e96d2d2d232546e29bd
smetzdev <metz@raum3.de>
2020-09-05 09:43:53 +0200

    Update message.stories.tsx

--- src/stories/2_components/message.stories.tsx
+++ src/stories/2_components/message.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Message from "@components/message"
 import { useFakeMessage } from "@fakeData"
+import { withPadding } from "../custom-decorators"
 
 export default {
   title: "Components/Message",
   component: Message,
+  decorators: [withPadding],
 }
 
 export const Base = () => {
